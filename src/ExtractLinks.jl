ba4416753fca8f1b975b728a8acd239298708d98
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-01-02 15:23:39 -0600

    Add docstring documenting a poor API

--- src/ExtractLinks.jl
+++ src/ExtractLinks.jl
@@ -22,13 +22,26 @@ const URI_TAGS = ["a/ping", "applet/archive", "applet/code", "applet/codebase",
 const LOOSE_REGEX = r"^(/|\w+://|\.)[-\w.\/?=&%+;:,#~\[\]@!$'()*]+$"
 const STRICT_REGEX = r"\w+:\/\/[-\w.\/?=&%+;:,#]*"
 
-function extract_links(body::AbstractString, root::AbstractString)
+"""
+    extract_links(html::AbstractString; root::AbstractString)
+
+Extract a list of links from an HTML document `html` with relative urls resolved assuming
+the page is hosted at `root`. `root` must be an absolute URL starting with http:// or
+https://. The links are returned as a vector of unique strings in arbitrary order.
+
+Makes a best effort to extract links from malformed HTML and should not throw due to
+malformed HTML.
+
+Due to the finniky nature of the web, this can only make a best effort, even if the HTML
+is well-formed. The exact huristics used are subject to change in a non-breaking release.
+"""
+function extract_links(html::AbstractString; root::AbstractString)
 
     startswith(root, r"https?://") || throw(ArgumentError("root must be an absolute URL starting with http:// or https://"))
-    # resolvereference will silently misbehave if not.
+    # resolvereference will sometimes silently misbehave if not.
 
     links = String[]
-    for node in PreOrderDFS(parsehtml(body).root)
+    for node in PreOrderDFS(parsehtml(html).root)
         if node isa HTMLElement
             t = String(tag(node))
             for (attr, val) in node.attributes
@@ -41,7 +54,7 @@ function extract_links(body::AbstractString, root::AbstractString)
             end
         end
     end
-    for m in eachmatch(ExtractLinks.STRICT_REGEX, body)
+    for m in eachmatch(ExtractLinks.STRICT_REGEX, html)
         push!(links, m.match)
     end
     unique!(links)
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 using Aqua
 
 @testset "Correctness (manual)" begin
-    links = extract_links(read(joinpath(@__DIR__, "pages", "www.julialang.org"), String), "https://www.julialang.org")
+    links = extract_links(read(joinpath(@__DIR__, "pages", "www.julialang.org"), String), root="https://www.julialang.org")
     for str in [
         "https://www.julialang.org/#tab-viz",
         "https://www.linkedin.com/company/the-julia-language",
@@ -26,13 +26,13 @@ using Aqua
         @test str ∉ links
     end
 
-    links = extract_links(read(joinpath(@__DIR__, "pages", "nano.org"), String), "https://nano.org")
+    links = extract_links(read(joinpath(@__DIR__, "pages", "nano.org"), String), root="https://nano.org")
     @test "https://nano.org/en/blog/author/Aneena Ann.Alexander" ∈ links
     @test "https://nano.org/en/blog/author/Aneena%20Ann.Alexander" ∉ links
 
     links = extract_links(
         read(joinpath(@__DIR__, "pages", "www.w3.org%F2TR%F2REC-html40%F2index%2Fattributes.html"), String),
-        "https://www.w3.org/TR/REC-html40/index/attributes.html")
+        root="https://www.w3.org/TR/REC-html40/index/attributes.html")
 
     @test "https://www.w3.org/TR/REC-html40/interact/forms.html#adef-accept" ∈ links
 end
@@ -41,14 +41,14 @@ end
 # semantic change, run `include("contrib/update.jl")` to update expected link files.
 @testset "Consistency" begin
     for file in readdir(joinpath(@__DIR__, "pages"))
-        actual = extract_links(read(joinpath(@__DIR__, "pages", file), String), "https://"*replace(file, "%2F" => "/"))
+        actual = extract_links(read(joinpath(@__DIR__, "pages", file), String), root="https://"*replace(file, "%2F" => "/"))
         expected = readlines(joinpath(@__DIR__, "expected", file))
         @test sort!(actual) == sort!(expected)
     end
 end
 
 @testset "Errors" begin
-    @test_throws ArgumentError extract_links("body", "root")
+    @test_throws ArgumentError extract_links("body"; root="root")
 end
 
 @testset "Code quality (Aqua.jl)" begin
